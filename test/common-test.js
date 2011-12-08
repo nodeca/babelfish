babb5c4f35526b15fb41dd7be289bfa03b34e48d
Aleksey V Zapparov <ixti@member.fsf.org>
2011-12-08 16:14:15 +0100

    Adds Common.filter test

--- test/common-test.js
+++ test/common-test.js
@@ -23,5 +23,17 @@ require('vows').describe('BabelFish.Common').addBatch({
     Assert.equal(params['foo.bar.baz'], 1);
     Assert.equal(params['bar.baz'],     2);
     Assert.equal(params['baz'],         3);
+  },
+
+  'filter': function () {
+    var result = Common.filter({
+      foo: 1,
+      bar: 2,
+    }, function even(val, key) {
+      return (0 === val % 2);
+    });
+
+    Assert.isUndefined(result.foo);
+    Assert.equal(result.bar, 2);
   }
 }).export(module);
