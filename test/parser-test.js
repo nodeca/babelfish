629e6f6701bf43d477f96e991f0adfa7f8df4384
Aleksey V Zapparov <ixti@member.fsf.org>
2011-12-12 15:44:29 +0100

    Adds parser test stubs

---
+++ test/parser-test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+var Assert = require('assert');
+var Parser = require('../lib/babelfish/parser');
+var Helper = require('./helper');
+
+
+require('vows').describe('BabelFish.Parser').addBatch({
+  'Parsing simple string': 'TBD',
+  'Parsing string with variable': 'TBD',
+  'Parsing string with plurals': 'TBD'
+}).export(module);
