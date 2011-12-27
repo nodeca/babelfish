e95183449295408750b2403d17a7b5b78e12797b
Vladimir Dronnikov <dronnikov@gmail.com>
2011-12-27 11:47:45 +0000

    attempt at use robuster compiler, failing

--- test/parser-test.js
+++ test/parser-test.js
@@ -7,9 +7,40 @@ var Helper = require('./helper');
 
 
 require('vows').describe('BabelFish.Parser').addBatch({
-  'Parsing simple string': 'TBD',
-  'Parsing string with variable': 'TBD',
-  'Parsing string with plurals': 'TBD',
+  'Parsing simple string': {
+    topic: function () {
+      return Parser.parse('Просто строка');
+    },
+    'results in simple string': function (result) {
+      Assert.deepEqual(result, [ { type: 'text', value: 'Просто строка' } ]);
+    }
+  },
+  'Parsing string with variable': {
+    topic: function () {
+      return Parser.parse('Complex string, with variable foo.bar.baz #{foo.bar.baz}');
+    },
+    'results in function': function (result) {
+      Assert.deepEqual(result, [ { value: 'Complex string, with variable foo.bar.baz ', type: 'text' }, { anchor: 'foo.bar.baz', type: 'variable' } ]);
+    }
+  },
+  'Parsing string with plurals': {
+    'empty plurals': {
+      topic: function () {
+        return Parser.parse('More complex string, with plurals foo_bar$baz.fu %{}:foo_bar$baz.fu');
+      },
+      'results in function': function (result) {
+        Assert.deepEqual(result, [ { value: 'More complex string, with plurals foo_bar$baz.fu %{}:foo_bar$baz.fu', type: 'text' } ]);
+      }
+    },
+    'singular form': {
+      topic: function () {
+        return Parser.parse('More complex string, with plurals foo_bar$baz.fu %{fou}:foo_bar$baz.fu');
+      },
+      'results in function': function (result) {
+        Assert.deepEqual(result, [ { value: 'More complex string, with plurals foo_bar$baz.fu ', type: 'text' }, { forms: [ 'fou' ], anchor: 'foo_bar', type: 'plural' }, { value: 'baz.fu', type: 'text' } ]);
+      }
+    }
+  },
   'MACROS_REGEXP': {
     'allows escaped argument separtor as part of argument': 'TBD',
     'allows escaped macros close char as part of argument': 'TBD'
--- test/pluralizer-test.js
+++ test/pluralizer-test.js
@@ -4,11 +4,45 @@
 var Assert = require('assert');
 var Pluralizer = require('../lib/babelfish/pluralizer');
 
+function helper(lang, expected, forms) {
+  function p(n) { return Pluralizer[lang](n, forms || ['one', 'other']); }
+  Assert.equal(p(0), expected[0]);
+  Assert.equal(p(1), expected[1]);
+  Assert.equal(p(2), expected[2]);
+  Assert.equal(p(5), expected[3]);
+  Assert.equal(p(11), expected[4]);
+  Assert.equal(p(21), expected[5]);
+}
 
 require('vows').describe('BabelFish.Pluralizer').addBatch({
   'en':  function () {
-    Assert.equal(Pluralizer.en(1, ['one', 'other']), 'one');
-    Assert.equal(Pluralizer.en(2, ['one', 'other']), 'other');
-    Assert.equal(Pluralizer.en(1, ['other']), 'other');
+    helper('en', [
+      'other', 'one', 'other', 'other', 'other', 'other'
+    ]);
+  },
+  'fr':  function () {
+    helper('fr', [
+      'one', 'one', 'other', 'other', 'other', 'other'
+    ]);
+  },
+  'de':  function () {
+    helper('de', [
+      'other', 'one', 'other', 'other', 'other', 'other'
+    ]);
+  },
+  'es':  function () {
+    helper('es', [
+      'other', 'one', 'other', 'other', 'other', 'other'
+    ]);
+  },
+  'ru':  function () {
+    helper('ru', [
+      'несколько', 'один', 'два', 'несколько', 'несколько', 'один'
+    ], ['один', 'два', 'несколько']);
+  },
+  'it':  function () {
+    helper('it', [
+      'other', 'one', 'other', 'other', 'other', 'other'
+    ]);
   }
 }).export(module);
