f5b4bd39fdc50fd707de3d5500a17865ebd4bbba
Alexey Zabrodskiy <elmigranto@gmail.com>
2012-12-22 13:07:47 +0600

    chg: cleaner test for issue-7

--- test/issues/issue-7.js
+++ test/issues/issue-7.js
@@ -6,11 +6,11 @@ var BabelFish = require('../..');
 
 
 module.exports = {
-  title: "#7: Zero-count in plurals.",
+  title: '#7: Numeric 0 in #{variable} should became "0"',
   fixed: true,
   test: function () {
     var i18n = new BabelFish('ru');
-    i18n.addPhrase('ru', 'nailsCount', 'У меня #{n} ((гвоздь|гвоздя|гвоздей)):n');
-    Assert.equal(i18n.translate('ru', 'nailsCount', {n: 0}), 'У меня 0 гвоздей');
+    i18n.addPhrase('ru', 'n', '#{n}');
+    Assert.equal(i18n.translate('ru', 'n', {n: 0}), '0');
   }
 };
