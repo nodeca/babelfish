a9af42f5e80a12c2913e97cdb52f57e8bc338583
Vitaly Puzrin <vitaly@rcdesign.ru>
2020-05-19 20:04:06 +0300

    rewrite doc builder to shelljs

--- support/build_doc.js
+++ support/build_doc.js
@@ -4,13 +4,13 @@
 
 /* eslint-env es6 */
 
-const { execSync } = require('child_process');
+const shell = require('shelljs');
 
-const head = execSync('git show-ref --hash HEAD', { encoding: 'utf8' }).slice(0, 6);
+shell.rm('-rf', 'doc');
+
+const head = shell.exec('git show-ref --hash HEAD').stdout.slice(0, 6);
 
 const link_format = `https://github.com/{package.repository}/blob/${head}/{file}#L{line}`;
 
-const result = execSync(`node node_modules/.bin/ndoc --link-format "${link_format}"`, { encoding: 'utf8' });
+shell.exec(`node node_modules/.bin/ndoc --link-format "${link_format}"`);
 
-/* eslint-disable no-console */
-console.log(result.trim());
